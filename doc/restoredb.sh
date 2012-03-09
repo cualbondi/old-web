118ae61a9fe5f6ba81166bfcd83b4f57df4e8d96
root <root@cualbondi.com.ar>
2012-03-09 19:19:01 +0100

    Modifique el restoredb.sh para darle permisos a web_colectivos

--- doc/restoredb.sh
+++ doc/restoredb.sh
@@ -7,12 +7,15 @@ echo "Eliminando DB..."
 dropdb $DATABASE
 
 echo "Creando nueva DB..."
-createdb $DATABASE --owner=postgres
+createdb $DATABASE --owner=web_colectivos
 
 echo "Instalando PostGIS..."
 createlang plpgsql $DATABASE
-psql -d $DATABASE -f /usr/share/postgresql/8.4/contrib/postgis-1.5/postgis.sql
-psql -d $DATABASE -f /usr/share/postgresql/8.4/contrib/postgis-1.5/spatial_ref_sys.sql
-psql -d $DATABASE -f /usr/share/postgresql/8.4/contrib/postgis_comments.sql
+psql -d $DATABASE -f /usr/share/postgresql/9.1/contrib/postgis-1.5/postgis.sql
+psql -d $DATABASE -f /usr/share/postgresql/9.1/contrib/postgis-1.5/spatial_ref_sys.sql
+psql -d $DATABASE -f /usr/share/postgresql/9.1/contrib/postgis_comments.sql
+
+psql -d $DATABASE -c 'GRANT ALL PRIVILEGES ON spatial_ref_sys TO web_colectivos'
+psql -d $DATABASE -c 'GRANT ALL PRIVILEGES ON geometry_columns TO web_colectivos'
 
 echo "Finalizado!"
--- settings.py
+++ settings.py
@@ -15,9 +15,9 @@ BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'geodjango',                      # Or path to database file if using sqlite3.
-        'USER': 'postgres',                      # Not used with sqlite3.
-        'PASSWORD': 'postgres',                  # Not used with sqlite3.
+        'NAME': 'dj',                      # Or path to database file if using sqlite3.
+        'USER': 'web_colectivos',                      # Not used with sqlite3.
+        'PASSWORD': 'soylawebcolectivos',                  # Not used with sqlite3.
         'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
