0a2a2fd5e1ea55bfad2273a6d03d06c91ce9b79c
root <root@cualbondi.com.ar>
2012-03-09 19:24:32 +0100

    Pasarle usuario como parametro al restodb.sh

--- doc/restoredb.sh
+++ doc/restoredb.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 DATABASE=$1
+USUARIO=$2
 echo "Iniciando script para la base de datos '$DATABASE'..."
 
 echo "Eliminando DB..."
 dropdb $DATABASE
 
 echo "Creando nueva DB..."
-createdb $DATABASE --owner=web_colectivos
+createdb $DATABASE --owner=$USUARIO
 
 echo "Instalando PostGIS..."
 createlang plpgsql $DATABASE
@@ -15,7 +16,7 @@ psql -d $DATABASE -f /usr/share/postgresql/9.1/contrib/postgis-1.5/postgis.sql
 psql -d $DATABASE -f /usr/share/postgresql/9.1/contrib/postgis-1.5/spatial_ref_sys.sql
 psql -d $DATABASE -f /usr/share/postgresql/9.1/contrib/postgis_comments.sql
 
-psql -d $DATABASE -c 'GRANT ALL PRIVILEGES ON spatial_ref_sys TO web_colectivos'
-psql -d $DATABASE -c 'GRANT ALL PRIVILEGES ON geometry_columns TO web_colectivos'
+psql -d $DATABASE -c "GRANT ALL PRIVILEGES ON spatial_ref_sys TO $USUARIO"
+psql -d $DATABASE -c "GRANT ALL PRIVILEGES ON geometry_columns TO $USUARIO"
 
 echo "Finalizado!"
