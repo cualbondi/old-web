3a4976f49241ef533a5a25abaa531140825e9aa9
Martin Zugnoni <martin@laptop.(none)>
2012-03-12 13:52:29 -0300

    Agregue nuevos Handlers para la API

--- apps/api/handlers.py
+++ apps/api/handlers.py
@@ -1,5 +1,51 @@
-from apps.core.models import Linea, Recorrido
+from apps.core.models import Ciudad, Linea, Recorrido
 from piston.handler import BaseHandler
+from piston.utils import rc
+from django.core.exceptions import ObjectDoesNotExist
+
+
+class CiudadHandler(BaseHandler):
+    allowed_methods = ['GET']
+    model = Ciudad
+    exclude = ()
+
+    def read(self, request, id_ciudad=None):
+        if id_ciudad is None:
+            return Ciudad.objects.all()
+        else:
+            try:
+                return Ciudad.objects.get(id=id_ciudad)
+            except ObjectDoesNotExist:
+                return rc.NOT_FOUND
+
+
+class CiudadLineaHandler(BaseHandler):
+    allowed_methods = ['GET']
+    exclude = ()
+
+    def read(self, request, id_ciudad=None):
+        if id_ciudad is None:
+            return rc.BAD_REQUEST
+        else:
+            try:
+                return Ciudad.objects.get(id=id_ciudad).lineas.all()
+            except ObjectDoesNotExist:
+                return rc.NOT_FOUND
+
+
+class CiudadRecorridoHandler(BaseHandler):
+    allowed_methods = ['GET']
+    exclude = ()
+
+    def read(self, request, id_ciudad=None):
+        if id_ciudad is None:
+            return rc.BAD_REQUEST
+        else:
+            try:
+                return Ciudad.objects.get(id=id_ciudad).recorridos.all()
+            except ObjectDoesNotExist:
+                return rc.NOT_FOUND
+
 
 class LineaHandler(BaseHandler):
     allowed_methods = ['GET']
@@ -11,7 +57,25 @@ class LineaHandler(BaseHandler):
             # Devolver la lista completa de Lineas
             return Linea.objects.all()
         else:
-            return Linea.objects.filter(id=id_linea)
+            try:
+                return Linea.objects.get(id=id_linea)
+            except ObjectDoesNotExist:
+                return rc.NOT_FOUND
+
+
+class LineaRecorridoHandler(BaseHandler):
+    allowed_methods = ['GET']
+    exclude = ()
+
+    def read(self, request, id_linea=None):
+        if id_linea is None:
+            return rc.BAD_REQUEST
+        else:
+            try:
+                l = Linea.objects.get(id=id_linea)
+                return Recorrido.objects.filter(linea=l)
+            except ObjectDoesNotExist:
+                return rc.NOT_FOUND
 
 
 class RecorridoHandler(BaseHandler):
@@ -24,4 +88,11 @@ class RecorridoHandler(BaseHandler):
             # Devolver la lista completa de Recorridos
             return Recorrido.objects.all()
         else:
-            return Recorrido.objects.filter(id=id_recorrido)
+            try:
+                return Recorrido.objects.get(id=id_recorrido)
+            except ObjectDoesNotExist:
+                return rc.NOT_FOUND
+
+
+
+
--- apps/api/urls.py
+++ apps/api/urls.py
@@ -2,7 +2,7 @@
 from django.conf.urls.defaults import patterns, url
 from django.http import HttpResponse
 from piston.resource import Resource
-from apps.api.handlers import LineaHandler, RecorridoHandler
+from apps.api.handlers import CiudadHandler, CiudadLineaHandler, CiudadRecorridoHandler, LineaHandler, LineaRecorridoHandler, RecorridoHandler
 
 def api_welcome(request):
     msg = """
@@ -18,15 +18,61 @@ def api_welcome(request):
     return HttpResponse(msg, content_type="text/plain")
 
 
+ciudades_handler = Resource(CiudadHandler, authentication=None)
+ciudades_lineas_handler = Resource(CiudadLineaHandler, authentication=None)
+ciudades_recorridos_handler = Resource(CiudadRecorridoHandler, authentication=None)
 lineas_handler = Resource(LineaHandler, authentication=None)
+lineas_recorridos_handler = Resource(LineaRecorridoHandler, authentication=None)
 recorridos_handler = Resource(RecorridoHandler, authentication=None)
 
 urlpatterns = patterns('',
     url(r'^$', api_welcome),
 
+    url(r'^ciudades/$', ciudades_handler),
+    url(r'^ciudades/(?P<id_ciudad>\d+)/$', ciudades_handler),
+    url(r'^ciudades/(?P<id_ciudad>\d+)/lineas/$', ciudades_lineas_handler),
+    url(r'^ciudades/(?P<id_ciudad>\d+)/recorridos/$', ciudades_recorridos_handler),
+
     url(r'^lineas/$', lineas_handler),
     url(r'^lineas/(?P<id_linea>\d+)/$', lineas_handler),
+    url(r'^lineas/(?P<id_linea>\d+)/recorridos/$', lineas_recorridos_handler),
 
     url(r'^recorridos/$', recorridos_handler),
     url(r'^recorridos/(?P<id_recorrido>\d+)/$', recorridos_handler),
 )
+
+
+"""
+ESTRUCTURA DE LAS URLs
+
+[DONE].com/api/ciudades/
+[DONE].com/api/ciudades/<id-ciudad>/
+[DONE].com/api/ciudades/<id-ciudad>/lineas/
+[DONE].com/api/ciudades/<id-ciudad>/recorridos/
+.com/api/ciudades/<id-ciudad>/comercios/
+.com/api/ciudades/<id-ciudad>/pois/
+
+[DONE].com/api/lineas/
+[DONE].com/api/lineas/<id-linea>/
+[DONE].com/api/lineas/<id-linea>/recorridos/
+
+.com/api/recorridos/
+    GET:    origen (40.737102,-73.990318)   \   Buscar recorridos que vayan de "origen" a "destino"
+            destino (40.737102,-73.990318)  /
+            radio_origen (200)  \   Tolerancia a caminar, tanto al subir como al bajar
+            radio_destino (350) /
+            query (Oeste 10 desde Olmos hasta centro)   =>  Busqueda de recorridos por nombre o lugares por dnd pasa
+            pagina (4)  =>  Dada una busqueda, los resultados se paginan y se devuelve la pagina "pagina"
+[DONE].com/api/recorridos/<id-recorrido>/
+.com/api/recorridos/<id-recorrido>/paradas/
+.com/api/recorridos/<id-recorrido>/paradas/<id-parada>/
+.com/api/recorridos/<id-recorrido>/paradas/<id-parada>/horarios/
+
+.com/api/usuarios/
+.com/api/usuarios/<id-usuario>/
+.com/api/usuarios/<id-usuario>/custom_pois/
+.com/api/usuarios/<id-usuario>/recorridos_favoritos/
+
+"""
+
+
