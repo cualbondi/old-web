cf55beae4694975b1c84165a673c261880627ff3
Martin Zugnoni <martin@laptop.(none)>
2012-05-09 16:25:54 -0300

    Agregue fixtures para provincia y ciudad de ARG_Adm + Adapte los modelos a los campos de ARG_Adm

--- util/load_fixtures.sh
+++ util/load_fixtures.sh
@@ -2,7 +2,8 @@
 
 python manage.py loaddata apps/usuarios/fixtures/usuario.json
 
-#python manage.py loaddata apps/catastro/fixtures/provincia.json
+python manage.py loaddata apps/catastro/fixtures/provincia_argadm.json
 python manage.py loaddata apps/core/fixtures/linea.json
 python manage.py loaddata apps/core/fixtures/recorrido.json
-#python manage.py loaddata apps/catastro/fixtures/ciudad.json
+python manage.py loaddata apps/catastro/fixtures/ciudad_argadm.json
+python manage.py intersectar_ciudad
