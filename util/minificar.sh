526efde808614ff129c2053c052cae25f0c95895
Martin Zugnoni <martin.zugnoni@gmail.com>
2013-02-10 11:37:44 -0300

    Add closure-compiler.jar + refactor minificar.sh

--- util/minificar.sh
+++ util/minificar.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 #cd cualbondi's git root
-java -jar util/closure-compiler.jar --js media/js/functions-2.0.js --js_output_file media/js/functions-2.0.js
-java -jar util/closure-compiler.jar --js media/js/vr-1.0.js --js_output_file media/js/vr-1.0.js
+java -jar util/closure-compiler.jar --js media/js/functions-2.0.js --js_output_file /tmp/functions-2.0.js
+mv /tmp/functions-2.0.js media/js/functions-2.0.js
+
+java -jar util/closure-compiler.jar --js media/js/vr-1.0.js --js_output_file /tmp/vr-1.0.js
+mv /tmp/vr-1.0.js media/js/vr-1.0.js
