d2e7a5cee680b3b5f190aa46f4101fda67f8d95a
Martin Zugnoni <martin.zugnoni@gmail.com>
2013-10-30 17:59:54 -0300

    Mapping de anuncios por ciudad

---
+++ anuncios.py
@@ -0,0 +1,28 @@
+"""Mapping de anuncios dividido por ciudad"""
+
+ANUNCIOS = {
+    'la-plata': [
+        {
+            'filename': 'FooBar3.gif',
+            'link_url': 'http://www.google.com'
+        },
+        {
+            'filename': 'FooBar4.gif',
+            'link_url': 'http://www.google.com'
+        },
+        {
+            'filename': 'FooBar5.gif',
+            'link_url': 'http://www.google.com'
+        },
+    ],
+    'buenos-aires': [
+        {
+            'filename': 'FooBar3.gif',
+            'link_url': 'http://www.google.com'
+        },
+        {
+            'filename': 'FooBar5.gif',
+            'link_url': 'http://www.google.com'
+        },
+    ]
+}
