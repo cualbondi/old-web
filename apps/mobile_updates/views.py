7b1e12997c2ef8a4aee85bcf8f22113f77a7d39f
jperelli <jperelli@gmail.com>
2014-03-02 16:29:08 -0300

    Agregando urls para actualizar info en la app mobile

---
+++ apps/mobile_updates/views.py
@@ -0,0 +1,65 @@
+from django.http import HttpResponse
+import json
+from apps.catastro.models import Ciudad
+from apps.core.models import Recorrido
+from apps.mobile_updates.models import Version
+from django.db.models import Max
+
+def json_response(func):
+    """
+    A decorator thats takes a view response and turns it
+    into json. If a callback is added through GET or POST
+    the response is JSONP.
+    """
+    def decorator(request, *args, **kwargs):
+        objects = func(request, *args, **kwargs)
+        if isinstance(objects, HttpResponse):
+            return objects
+        try:
+            data = json.dumps(objects)
+            if 'callback' in request.REQUEST:
+                # a jsonp response!
+                data = '%s(%s);' % (request.REQUEST['callback'], data)
+                return HttpResponse(data, "text/javascript")
+        except:
+            data = json.dumps(str(objects))
+        return HttpResponse(data, "application/json")
+    return decorator
+
+
+@json_response
+def ciudades_package(request):
+    return [
+        {
+            'slug'  : c.slug,
+            'nombre': c.nombre,
+            'latlng': list(c.centro.coords)
+        } 
+        for c in Ciudad.objects.all()
+    ]
+
+@json_response
+def recorridos_package(request):
+    ci_sl = request.GET.get("ciudad", "")
+    ci = Ciudad.objects.get(slug=ci_sl)
+    return [
+        {
+            'li': r.linea.nombre,
+            're': r.nombre,
+            'id': r.id
+        }
+        for r in Recorrido.objects.all().select_related('linea__ciudad').filter(linea__ciudad=ci)
+    ]
+
+@json_response
+def versiones(request):
+#    return [
+#        {
+#            'tipo'     : v.tipo,
+#            'timestamp': v.timestamp,
+#            'name'     : v.name,
+#            'noticia'  : v.noticia
+#        }
+#        for v in 
+	return Version.objects.values('tipo').annotate(timestamp_max=Max('timestamp'))
+#    ]
