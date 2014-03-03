2697ae579f2055e748b07fd972603df92626de2a
jperelli <jperelli@gmail.com>
2014-03-02 21:01:30 -0300

    cambiando url de versiones

--- apps/mobile_updates/urls.py
+++ apps/mobile_updates/urls.py
@@ -4,5 +4,5 @@ from django.conf.urls.defaults import patterns
 urlpatterns = patterns('',
      (r'^package_recorridos$', 'apps.mobile_updates.views.recorridos_package'),
      (r'^package_ciudades$', 'apps.mobile_updates.views.ciudades_package'),
-     (r'^$', 'apps.mobile_updates.views.versiones'),
+     (r'^versiones$', 'apps.mobile_updates.views.versiones'),
 )
