786d018529c4fe3d07ae6e59dd948d4d7a7f67a0
jperelli <jperelli@gmail.com>
2014-04-26 23:10:47 -0300

    Me olvide esto comentado

--- apps/editor/models.py
+++ apps/editor/models.py
@@ -135,7 +135,7 @@ class RecorridoProposed(models.Model):
             rp.logmoderacion_set.create(created_by=user, newStatus='R')
         self.logmoderacion_set.create(created_by=user, newStatus='S')
         
-        #call_command('crear_thumbs', recorrido_id=self.recorrido.id)
+        call_command('crear_thumbs', recorrido_id=self.recorrido.id)
         
         # Notificacion por facebook
         token = urllib2.urlopen('https://graph.facebook.com/oauth/access_token?client_id='+settings.SOCIAL_AUTH_FACEBOOK_KEY+'&client_secret='+settings.SOCIAL_AUTH_FACEBOOK_SECRET+'&grant_type=client_credentials').read().split('access_token=')[1]
