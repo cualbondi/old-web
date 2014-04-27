a7e4fcb44284b52a2193836c5986ad20130ecfc9
jperelli <jperelli@gmail.com>
2014-04-26 23:52:44 -0300

    mejorando la visualizacion del nombre

--- apps/editor/models.py
+++ apps/editor/models.py
@@ -78,7 +78,14 @@ class RecorridoProposed(models.Model):
             return loglist[0].created_by
         else:
             return AnonymousUser()
-
+        
+    def get_pretty_user(self):
+        user = self.get_moderacion_last_user()
+        if user.first_name or user.last_name:
+            return user.first_name + " " + user.last_name
+        else:
+            return user.username
+        
     def get_fb_uid(self):
         last = self.get_moderacion_last_user()
         if last != AnonymousUser():
