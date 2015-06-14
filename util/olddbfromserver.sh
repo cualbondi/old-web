2fe6724a148c284b893ee0f18f12dcaa35258f10
Julian <jperelli@gmail.com>
2015-06-14 15:24:35 -0300

    Adding copydb script

--- util/olddbfromserver.sh
+++ util/olddbfromserver.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
+
+ID=$RANDOM
+
 echo "dumping b2zipping"
-ssh cualbondi@cualbondi.com.ar "su root -c \"su postgres -c \\\"pg_dump --inserts -trecorrido -tlinea -tramal -tciudad -tciudad_linea -c colectivos -Upostgres -hlocalhost | bzip2 > /tmp/dump.bz2\\\"\""
+ssh -t cualbondi.com.ar "su root -c \"su postgres -c \\\"pg_dump --inserts -c geocualbondidb -Upostgres | bzip2 > /tmp/dump-$ID.bz2\\\"\""
 
 echo "sending file"
-rsync -v -e ssh cualbondi@cualbondi.com.ar:/tmp/dump.bz2 ~
+rsync -v -e ssh cualbondi.com.ar:/tmp/dump-$ID.bz2 /tmp/dump-$ID.bz2
 
-echo "bunzipping & inserting in database in remote host"
-bunzip2 dump.bz2 -c | psql -Upostgres -h127.0.0.1 colectivos
-
-# NOTA: poner bien el nombre de la BD y de las tablas a backupear
+echo "bunzipping & inserting in database in local host"
+bunzip2 /tmp/dump-$ID.bz2 -c | vagrant ssh -c "sudo -u postgres psql -Upostgres cualbondi"
