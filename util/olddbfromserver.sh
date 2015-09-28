af5d55b1fe17902864a72e6b4643be3280f72abd
Julian <jperelli@gmail.com>
2015-09-28 19:34:58 -0300

    get db in postgres custom format, to be faster

--- util/olddbfromserver.sh
+++ util/olddbfromserver.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 echo "[$(date -Is -u)] Dumping database"
-ssh -t cualbondi.com.ar "su root -c \"sudo -u postgres pg_dump -Fc -f /tmp/dump.pgbkp geocualbondidb\""
+ssh -t cualbondi.com.ar "su root -c \"sudo -u postgres 'pg_dump -Fc -Z9 -d geocualbondidb > /tmp/dump.pgbkp'\""
 
 echo "[$(date -Is -u)] Getting file"
 rsync -v -e ssh cualbondi.com.ar:/tmp/dump.pgbkp /tmp/dump.pgbkp
 
 echo "[$(date -Is -u)] Inserting in database in local host"
-sudo -u postgres pg_restore /tmp/dump.pgbkp
+sudo -u postgres pg_restore -C -c -Fc -j8 /tmp/dump.pgbkp
