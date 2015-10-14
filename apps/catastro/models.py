d936d80806a3900a5d4673b46688180a133145aa
Julian <jperelli@gmail.com>
2015-10-14 17:50:51 -0300

    cleaning up

--- apps/catastro/models.py
+++ apps/catastro/models.py
@@ -92,10 +92,10 @@ class Ciudad(models.Model):
     slug = models.SlugField(max_length=120, blank=True, null=False)
     provincia = models.ForeignKey(Provincia)
     activa = models.BooleanField(blank=True, null=False, default=False)
-    img_panorama = models.ImageField(max_length=200, upload_to='ciudad', blank=True, null=True)
-    img_cuadrada = models.ImageField(max_length=200, upload_to='ciudad', blank=True, null=True)
 
     # Opcionales
+    img_panorama = models.ImageField(max_length=200, upload_to='ciudad', blank=True, null=True)
+    img_cuadrada = models.ImageField(max_length=200, upload_to='ciudad', blank=True, null=True)
     variantes_nombre = models.CharField(max_length=150, blank=True, null=True)
     recorridos = models.ManyToManyField('core.Recorrido', blank=True, null=True)
     lineas = models.ManyToManyField('core.Linea', blank=True, null=True)
