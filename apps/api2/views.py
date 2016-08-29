443547b8148d389331c56f2c2475ea4948238fa0
Julian <jperelli@gmail.com>
2016-08-29 20:19:58 -0300

    Cambiando parametro 'ciudad' por 'c' para que sea consistente, en todos lados igual

--- apps/api2/views.py
+++ apps/api2/views.py
@@ -128,20 +128,20 @@ class GeocoderViewSet(viewsets.GenericViewSet):
         Parámetros querystring
 
          - `q` obligatorio: string a geobuscar
-         - `ciudad` opcional: slug de la ciudad donde buscar
+         - `c` opcional: slug de la ciudad donde buscar
 
         Busca el valor del parámetro `q`
         usando varias fuentes según el formato del string de búsqueda
 
-         - **Geocoder** [Google] (ej: [12 1234](/api/v2/geocoder/?q=12%201234&ciudad=la-plata) / [12 n 1234](/api/v2/geocoder/?q=12%20n%201234&ciudad=la-plata) / [centenario 1234](/api/v2/geocoder/?q=centenario%20n%201234&ciudad=la-plata))
-         - **Intersección de calles** [OSM] (ej: [12 y 62](/api/v2/geocoder/?q=12%20y%2062&ciudad=la-plata) / perón y alvarez)
+         - **Geocoder** [Google] (ej: [12 1234](/api/v2/geocoder/?q=12%201234&c=la-plata) / [12 n 1234](/api/v2/geocoder/?q=12%20n%201234&c=la-plata) / [centenario 1234](/api/v2/geocoder/?q=centenario%20n%201234&c=la-plata))
+         - **Intersección de calles** [OSM] (ej: [12 y 62](/api/v2/geocoder/?q=12%20y%2062&c=la-plata) / perón y alvarez)
          - **POI (Point Of Interest)** [OSM y Cualbondi] (ej: plaza rocha / hospital)
          - **Zona (Barrio / Ciudad)** [Cualbondi] (ej: berisso / colegiales) (devuelve geocentro)
 
         El geocoder usa varias fuentes y técnicas, entre ellas fuzzy search.
         Por esto, devuelve un valor de "precision" para cada registro.
 
-        Utiliza el parámetro `ciudad` para dar mas contexto al lugar
+        Utiliza el parámetro `c` (ciudad) para dar mas contexto al lugar
         donde se está buscando. Esto ayuda a evitar ambigüedad en la búsqueda
         ya que puede haber dos calles que se llamen igual en distintas ciudades
         pero no restringe la búsqueda a esa ciudad (sólo altera la precisión)
@@ -150,7 +150,7 @@ class GeocoderViewSet(viewsets.GenericViewSet):
 
     def list(self, request):
         q = request.query_params.get('q', None)
-        ciudad_actual_slug = request.query_params.get('ciudad', None)
+        ciudad_actual_slug = request.query_params.get('c', None)
         if q is None:
             raise exceptions.ValidationError(
                 {'detail': 'expected \'q\' parameter'}
